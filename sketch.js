bd179494763c7e16d201a0399359c3700d7d364d
DHAIRYASINDHWANI <72784816+DHAIRYASINDHWANI@users.noreply.github.com>
2020-11-05 16:46:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1,box2,box3,box4,box5, pig1,pig2,ground1,log1,log2,log3,log4,bird;
+var bgimg;
+var platform;
+var Log6;
+var chain;
+function preload(){
+    bgimg = loadImage("sprites/bg.png");
+}
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    platform = new Ground(105,305,300,170);
+    ground1 = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    box5 = new Box(810,160,70,70);
+
+    pig1 = new Pig(810, 350);
+    pig2 = new Pig(810, 220);
+
+    log1 = new Log(810,260,300, PI/2);
+    log2 = new Log(870,120,130, -PI/7);
+    log3 =  new Log(810,180,300, PI/2);
+    log4 = new Log(760,120,130, PI/7);
+  
+Log6 = new Log(230,180,80,PI/2);
+    bird = new Bird(100,100);
+
+chain = new Chain(bird.body,Log6.body);
+
+}
+
+function draw(){
+    background(bgimg);
+
+    Engine.update(engine);
+
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
+
+    platform.display();
+    
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+
+    ground1.display();
+
+    pig1.display();
+    pig2.display();
+    
+    log1.display();
+    log2.display();
+    log3.display();
+    log4.display();
+
+    bird.display();
+
+    Log6.display();
+    chain.display();
+}
